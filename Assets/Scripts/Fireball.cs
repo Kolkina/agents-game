08f0695af39a08d24ce9e643719766ec9f5d42af
Kolkina <asanirel@gmail.com>
2016-10-25 14:45:47 +0200

    Branch commit

--- Assets/Scripts/Fireball.cs
+++ Assets/Scripts/Fireball.cs
@@ -3,10 +3,17 @@ using System.Collections;
 
 public class Fireball : MonoBehaviour {
 
+	private float velocity = 4f;
+	private Vector3 dir = Vector3.up;
+	
+	public void SetDirection(Vector3 newDir)
+	{
+		dir = newDir;
+	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate(Vector3.up * 4 * Time.deltaTime);
+		transform.Translate(dir * velocity * Time.deltaTime);
 		if(transform.position.y > 10)
 			Destroy(this);
 	}
--- Assets/Scripts/ProjectileSpawner.cs
+++ Assets/Scripts/ProjectileSpawner.cs
@@ -4,16 +4,29 @@ using System.Collections;
 public class ProjectileSpawner : MonoBehaviour {
 
 	public GameObject projectile;
+	public Direction dir;
+	public enum Direction {Left,Right,Up,Down};
 
 	private float timeSinceFire = 0f;
 	private float frequency = 0.5f;
+	private Vector3 fireballDir = Vector3.up;
+	
+	void Start () {
+		switch (dir)
+		{
+			case Direction.Left:
+				fireballDir = -Vector3.right;
+				break;
+		}
+	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 		if(timeSinceFire > frequency)
 		{
-			Instantiate (projectile, transform.position, Quaternion.identity);
+			GameObject fb = Instantiate (projectile, transform.position, Quaternion.identity) as GameObject;
+			fb.GetComponent<Fireball>().SetDirection(fireballDir);
 			timeSinceFire = 0;
 		}
 		else
